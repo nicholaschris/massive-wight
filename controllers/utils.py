cd0528009baf3559bb8bdaa2c966d215ed700230
Nicholas <nicholaschristopherpringle@gmail.com>
2013-12-02 21:57:22 +0100

    Added .gitignore

--- controllers/utils.py
+++ controllers/utils.py
@@ -512,6 +512,17 @@ class ForumHandler(BaseHandler):
         self.template_value['navbar'] = {'forum': True}
         self.render('forum.html')
 
+### Added FaqHandler ###
+class FaqHandler(BaseHandler):
+    """Handler for FAQ page."""
+    def get(self):
+        """Handles GET requests."""
+        if not self.personalize_page_and_get_enrolled():
+            return
+
+        self.template_value['navbar'] = {'faq': True }
+        self.render('faq.html')
+###
 
 class StudentProfileHandler(BaseHandler):
     """Handles the click to 'Progress' link in the nav bar."""
